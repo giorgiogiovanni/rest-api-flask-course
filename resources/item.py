cf5f989d8018f5468ca01eb409705cc965d670d1
Jorge Bailon <jorge.bailon@htecgroup.com>
2025-06-24 10:34:08 +0200

    UPDATED to Final One-to-may Rel Lesson

--- resources/item.py
+++ resources/item.py
@@ -1,5 +1,3 @@
-import uuid
-from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from sqlalchemy.exc import SQLAlchemyError
@@ -8,38 +6,45 @@ from db import db
 from models import ItemModel
 from schemas import ItemSchema, ItemUpdateSchema
 
+blp = Blueprint("Items", "items", description="Operations on items")
 
-blp = Blueprint("Items", __name__, description="Operation on items")
 
 @blp.route("/item/<string:item_id>")
 class Item(MethodView):
-
-    @blp.response(200,ItemSchema)
+    @blp.response(200, ItemSchema)
     def get(self, item_id):
         item = ItemModel.query.get_or_404(item_id)
         return item
 
-
     def delete(self, item_id):
         item = ItemModel.query.get_or_404(item_id)
-        raise NotImplementedError("Deleting an item is not implemented")
-
+        db.session.delete(item)
+        db.session.commit()
+        return {"message": "Item deleted."}
 
     @blp.arguments(ItemUpdateSchema)
     @blp.response(200, ItemSchema)
     def put(self, item_data, item_id):
-        item = ItemModel.query.get_or_404(item_id)
-        raise NotImplementedError("Updating an item is not implemented")
+        item = ItemModel.query.get(item_id)
+
+        if item:
+            item.price = item_data["price"]
+            item.name = item_data["name"]
+        else:
+            item = ItemModel(id=item_id, **item_data)
+
+        db.session.add(item)
+        db.session.commit()
+
+        return item
 
 
 @blp.route("/item")
 class ItemList(MethodView):
-
     @blp.response(200, ItemSchema(many=True))
     def get(self):
-        # db.session.g
-        return items.values()
-    
+        return ItemModel.query.all()
+
     @blp.arguments(ItemSchema)
     @blp.response(201, ItemSchema)
     def post(self, item_data):
@@ -49,6 +54,6 @@ class ItemList(MethodView):
             db.session.add(item)
             db.session.commit()
         except SQLAlchemyError:
-            abort(500, message="An error occurred while inserting item in the db")
+            abort(500, message="An error occurred while inserting the item.")
 
         return item
--- resources/store.py
+++ resources/store.py
@@ -1,19 +1,17 @@
-import uuid
-from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from sqlalchemy.exc import SQLAlchemyError, IntegrityError
 
+from db import db
 from models import StoreModel
 from schemas import StoreSchema
-from db import db
 
 
-blp = Blueprint("stores", __name__, description="Operations on stores")
+blp = Blueprint("Stores", "stores", description="Operations on stores")
+
 
 @blp.route("/store/<string:store_id>")
 class Store(MethodView):
-
     @blp.response(200, StoreSchema)
     def get(self, store_id):
         store = StoreModel.query.get_or_404(store_id)
@@ -21,17 +19,17 @@ class Store(MethodView):
 
     def delete(self, store_id):
         store = StoreModel.query.get_or_404(store_id)
-        raise NotImplementedError("Deleting a Store is not implemented")
-
+        db.session.delete(store)
+        db.session.commit()
+        return {"message": "Store deleted"}, 200
 
 
 @blp.route("/store")
 class StoreList(MethodView):
-    
     @blp.response(200, StoreSchema(many=True))
-    def get(self):  
-        return stores.values()
-    
+    def get(self):
+        return StoreModel.query.all()
+
     @blp.arguments(StoreSchema)
     @blp.response(201, StoreSchema)
     def post(self, store_data):
@@ -42,7 +40,7 @@ class StoreList(MethodView):
         except IntegrityError:
             abort(
                 400,
-                message="A store with that name already exists"
+                message="A store with that name already exists.",
             )
         except SQLAlchemyError:
             abort(500, message="An error occurred creating the store.")
