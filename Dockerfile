9e5247d08036c7e4c873da432497ef7b5a48fdd7
Jorge Bailon <jorge.bailon@htecgroup.com>
2025-07-23 13:13:08 +0200

    migrate to use in the web service

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,5 @@ WORKDIR /app
 COPY requirements.txt .
 RUN pip install -r requirements.txt
 COPY . .
+CMD ["flask","db","upgrade"]
 CMD ["flask","run","--host","0.0.0.0"]
--- app.py
+++ app.py
@@ -3,14 +3,13 @@ from flask_smorest import Api
 from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
 
-import models
-
 from db import db
 from blocklist import BLOCKLIST
+
+from resources.users import blp as UserBlueprint
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
 from resources.tag import blp as TagBlueprint
-from resources.users import blp as UserBlueprint
 
 
 def create_app(db_url=None):
@@ -31,21 +30,45 @@ def create_app(db_url=None):
     api = Api(app)
 
     app.config["JWT_SECRET_KEY"] = "my-secret-key"
-    # secrets.SystemRandom().getrandbits(128) do once and 
-    
     jwt = JWTManager(app)
+
+    # @jwt.additional_claims_loader
+    # def add_claims_to_jwt(identity):
+    #     # TODO: Read from a config file instead of hard-coding
+    #     if identity == 1:
+    #         return {"is_admin": True}
+    #     return {"is_admin": False}
 
     @jwt.token_in_blocklist_loader
     def check_if_token_in_blocklist(jwt_header, jwt_payload):
         return jwt_payload["jti"] in BLOCKLIST
-    
-    @jwt.revoked_token_loader
-    def revoked_token_callbakc(jwt_header, jwt_payload):
-        return(
+
+    @jwt.expired_token_loader
+    def expired_token_callback(jwt_header, jwt_payload):
+        return (
+            jsonify({"message": "The token has expired.", "error": "token_expired"}),
+            401,
+        )
+
+    @jwt.invalid_token_loader
+    def invalid_token_callback(error):
+        return (
             jsonify(
-                {"description": "The token has been revoked","error":"token_revoked"}
+                {"message": "Signature verification failed.", "error": "invalid_token"}
             ),
-            401
+            401,
+        )
+
+    @jwt.unauthorized_loader
+    def missing_token_callback(error):
+        return (
+            jsonify(
+                {
+                    "description": "Request does not contain an access token.",
+                    "error": "authorization_required",
+                }
+            ),
+            401,
         )
 
     @jwt.needs_fresh_token_loader
@@ -53,56 +76,26 @@ def create_app(db_url=None):
         return (
             jsonify(
                 {
-                    "description": "The token is not fresh",
-                    "error": "fresh_token_required"
+                    "description": "The token is not fresh.",
+                    "error": "fresh_token_required",
                 }
             ),
-            401
-        )
-
-    @jwt.additional_claims_loader
-    def add_claims_to_jwt(identity):
-        print(identity)
-        if identity == "1":
-            return {"is_admin":True}
-        return {"is_admin":False}
-
-    # Configurations for the JWT
-    @jwt.expired_token_loader
-    def expired_token_callback(jwt_header, jwt_payload):
-        return(
-            jsonify({"message":"The token has expired", "error":"token_expired"}),
-            401,
-        )
-    
-    @jwt.invalid_token_loader
-    def invalid_token_callback(error):
-        return(
-            jsonify({"message": "Signature verification failed", "error":"invalid_token"}),
             401,
         )
 
-    @jwt.unauthorized_loader
-    def missing_token_callback(error):
-        return(
+    @jwt.revoked_token_loader
+    def revoked_token_callback(jwt_header, jwt_payload):
+        return (
             jsonify(
-                {
-                    "description": "Request does not contain an access token",
-                    "error":"authorization_required"
-                }
+                {"description": "The token has been revoked.", "error": "token_revoked"}
             ),
             401,
         )
 
 
-
-    # Not needed since use flask-migrate
-    # with app.app_context():
-    #     db.create_all()
-
+    api.register_blueprint(UserBlueprint)
     api.register_blueprint(ItemBlueprint)
     api.register_blueprint(StoreBlueprint)
     api.register_blueprint(TagBlueprint)
-    api.register_blueprint(UserBlueprint)
 
     return app
